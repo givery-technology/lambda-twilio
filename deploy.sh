70e31b1311a8a3e2a4917ce8af32846ad9c47fab
skohar <skohar@users.noreply.github.com>
2016-02-05 12:26:59 +0900

    Add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
-sbt assembly
-aws s3 cp target/scala-2.11/lambda-twilio-assembly-0.1-SNAPSHOT.jar s3://$S3_BUCKET/
-aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $S3_BUCKET --s3-key home-ip-assembly-0.1-SNAPSHOT.jar
+#!/bin/bash -eu
+sbt assembly && \
+aws lambda update-function-code \
+  --function-name $FUNCTION_NAME \
+  --zip-file fileb://target/scala-2.11/lambda-twilio-assembly-0.1-SNAPSHOT.jar
+
+#aws lambda create-function \
+#  --function-name $FUNCTION_NAME \
+#  --zip-file fileb://target/scala-2.11/lambda-twilio-assembly-0.1-SNAPSHOT.jar \
+#  --role arn:aws:iam::$AWS_ID:role/$LAMBDA_ROLE \
+#  --handler com.github.skohar.lambdatwilio.App::handler \
+#  --runtime java8 \
+#  --timeout 30 \
+#  --memory-size 512
