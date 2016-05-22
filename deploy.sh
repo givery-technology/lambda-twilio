000000906eaff64ae9fdcc07f81e93b6349a2cc2
shok4a <shok4a@users.noreply.github.com>
2016-05-22 17:58:53 +0900

    Deploy function via S3

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -eu
 sbt clean &&\
 sbt assembly && \
+aws s3 cp target/scala-2.11/lambda-twilio-assembly-0.1-SNAPSHOT.jar s3://$BUCKET_NAME/ && \
 aws lambda update-function-code \
   --function-name $FUNCTION_NAME \
-  --zip-file fileb://target/scala-2.11/lambda-twilio-assembly-0.1-SNAPSHOT.jar
+  --s3-bucket $S3_BUCKET --s3-key lambda-twilio-assembly-0.1-SNAPSHOT.jar
 
 #aws lambda create-function \
 #  --function-name $FUNCTION_NAME \
